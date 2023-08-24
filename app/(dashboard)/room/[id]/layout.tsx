7a45f3251544e534b7cc56e410913ae2aa0d4d58
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-08-24 21:19:07 +0530

    room-height-fix

--- app/(dashboard)/room/[id]/layout.tsx
+++ app/(dashboard)/room/[id]/layout.tsx
@@ -4,7 +4,7 @@ const RoomLayout = ({children}) => {
   return (
     <div className="h-full w-full">
       Chess Board : {children}
-      <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center justify-center h-full w-full">
         <div className=" border border-black">
           <Chessboard />
         </div>
