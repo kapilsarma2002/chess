ec3a59c4d19dffba28e3a6f7fe73e640b9722902
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-09-07 21:19:31 +0530

    added timer

--- app/(dashboard)/room/[id]/layout.tsx
+++ app/(dashboard)/room/[id]/layout.tsx
@@ -1,10 +1,12 @@
 import Board from '@/components/ChessBoard'
+import Timer from '@/components/Timer'
 
 const RoomLayout = ({ children }) => {
   return (
     <div className="flex flex-col items-center justify-center h-[calc(100vh-60px)] border border-black">
       <div className="border border-black">
         <Board />
+        <Timer />
       </div>
     </div>
   )
